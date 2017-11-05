0276764d3f481b9a4dd12f49d1e179a2eb370e8d
lizhi <799643644@qq.com>
2017-11-05 22:37:21 +0800

    退出登录完成

--- public/src/utils.js
+++ public/src/utils.js
@@ -1,12 +1,26 @@
 define(['jquery'],function($){
+    //检测登录
       $.ajax({
             url:'/api/employee/checkRootLogin',
             type:'get',
             success:function (info) {
                 // console.log(info);
                 if(info.error) {
-                    location.href = '/login.html';
+                    // location.href = '/login.html';
                 }
             }
         })
+
+      $('.logout').on('click',function() {
+            $.ajax({
+                url:'/api/employee/employeeLogout',
+                type:'get',
+                success:function(info) {
+                    if(info.success) {
+                        location.href='/login.html';
+                    }
+                }
+            })
+      })
+    //退出登录
 })
